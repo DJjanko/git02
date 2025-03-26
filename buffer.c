082057fe1a4f0bb2f9ef1b728a03503dbabc2bc3
DJjanko <jankladusek7@gmail.com>
2025-03-26 20:32:11 +0100

    buffer print

--- buffer.c
+++ buffer.c
@@ -55,4 +55,23 @@ void buffer_fill(uint16_t *buffer){
 }
 
 void buffer_print(uint16_t *buffer){
+    void *pointer = (uint8_t*)buffer;
+    //printf("--------------------------\n");
+    printf("%c\n",*(char*)((char *)pointer));
+
+    printf("%c\n",*(char*)((char *)pointer+1));
+
+    printf("%c\n",*(char*)((char *)pointer+2));
+
+    printf("%f\n",*(float *)((char*)pointer+3));
+
+    printf("%d\n",*(int32_t *)((char*)pointer+7));
+
+    printf("%c\n",*(char*)((char *)pointer+11));
+
+    printf("%c\n",*(char*)((char *)pointer+12));
+
+    printf("%c\n",*(char*)((char *)pointer+13));
+
+
 }
