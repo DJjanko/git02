fedaecf739d02386a0a033f99b24d6ba16586d02
DJjanko <jankladusek7@gmail.com>
2025-03-26 20:31:37 +0100

    buffer free

--- buffer.c
+++ buffer.c
@@ -5,7 +5,9 @@ uint16_t *buffer_alloc(size_t size){
     uint16_t * buffer = malloc(size);
     return buffer;
 }
+
 void buffer_free(uint16_t *buffer){
+    free(buffer);
 }
 
 void buffer_fill(uint16_t *buffer){
